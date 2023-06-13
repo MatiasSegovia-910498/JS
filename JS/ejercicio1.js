a53e807bc07dc9b06800f29cb386da04c10be3f2
MatiasSegovia-910498 <matiassegovia855@gmail.com>
2023-06-13 18:47:12 -0300

    ejercicios

--- JS/ejercicio1.js
+++ JS/ejercicio1.js
@@ -1,9 +1,19 @@
 
+var meses = [
+    {"mes":"enero","ingreso" : 500,"egreso": 500},
+    {"mes":"febrero","ingreso" : 2000,"egreso": 2000},
+    {"mes":"marzo","ingreso" : 84683,"egreso": 2000},
+    {"mes":"abril","ingreso" : 15000,"egreso": 1533},
+    {"mes":"mayo","ingreso" : 1500,"egreso": 5400},
+    {"mes":"junio","ingreso" : 43000,"egreso": 54000},
+    {"mes":"julio","ingreso" : 43000,"egreso": 4500},
+    {"mes":"agosto","ingreso" : 78000,"egreso": 7800},
+    {"mes":"septiembre","ingreso" : 1800,"egreso": 95000},
+    {"mes":"octubre","ingreso" : 48000,"egreso": 9400},
+    {"mes":"noviembre","ingreso" : 35000,"egreso": 53000},
+    {"mes":"diciembre","ingreso" : 3800,"egreso": 34000}
+ ]
+for ( var i=0 ; i<meses.length ; i++) {
+    console.log ("su balance de " + meses [i].mes+ " es $"+ (meses[i].ingreso-meses[i].egreso))
+}
 
-        var edad = prompt("ingrese su edad")
-        
-        if (edad>=18) {
-            alert("Bienvenido")
-        } else {
-            alert("Usted es menor de edad, no puede ingresar a este sitio")
-        }
--- JS/ejercicio2.js
+++ JS/ejercicio2.js
@@ -1,9 +1,8 @@
-var usuario = prompt("ingrese su usuario")
-var clave = prompt ("ingrese su clave")
-
-        
-if (usuario == "admi" && clave == "1234") {
-    alert("Bienvenido")
-} else {
-    alert("Su usuario o clave son incorrectas")
+var planes = [
+    {"plan":"plan01","capital" : 15000,"plazo": 30, "tasa": 0.15},
+    {"plan":"plan02","capital" : 30000,"plazo": 180, "tasa": 0.10},
+    {"plan":"plan03","capital" : 48500,"plazo": 60, "tasa": 0.23}
+]
+for ( var i=0 ; i<planes.length ; i++) {
+    alert ("su tasa de interes de " + planes [i].plan + " es "+ ((planes[i].capital*planes[i].plazo*planes[i].tasa) / 100 ))
 }
